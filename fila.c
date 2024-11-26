f6dee52a53ababcb31394c2170d337519bdaf50a
Bruna-righi <righi.bruna@acad.ufsm.br>
2024-11-26 13:23:02 -0300

    implementação das funções de fila

--- fila.c
+++ fila.c
@@ -1,4 +1,44 @@
 //
 // Created by amand on 30/10/2024.
 //
+#include <stdlib.h>
 #include "fila.h"
+
+
+Fila* filaCria(void){
+    Fila* temp = (Fila*) malloc(sizeof(Fila));
+    temp->n = 0;
+    temp->ini = 0;
+    return temp;
+}
+
+bool filaVazia(Fila* filaClientes){
+    if(filaClientes->n == 0) return true;
+    else return false;
+}
+
+void filaInsere(Fila* filaClientes, int quantos){
+    int fim = (filaClientes->ini + filaClientes->n)%N;
+    if(filaClientes->n == N){
+        printf("Capacidade da fila estourou, não foi possível inserir!\n");
+        return;
+    }
+    filaClientes->vet[fim]=quantos;
+    filaCLientes->n++;
+}
+
+int filaRetira(Fila* filaClientes){
+    if(!filaVazia(filaClientes)){
+        int temp = filaClientes->vet[filaClientes->ini];
+        filaClientes->ini=(filaClientes->ini + 1)%N;
+        filaClientes->n--;
+        return v;
+    }else {
+        printf("Fila vazia, nada a remover!");
+        return 0;
+    }
+}
+
+void filaLibera(Fila* filaClientes){
+    free(filaClientes);
+}
--- fila.h
+++ fila.h
@@ -6,3 +6,18 @@
 #define FILA_H
 
 #endif //FILA_H
+
+#define N 100
+
+typedef struct fila {
+    int n; 
+    int ini;
+    int vet[N];
+}Fila;
+
+Fila* filaCria(void);
+
+bool filaVazia(Fila* filaClientes);
+void filaInsere(Fila* filaClientes, int quantos);
+int filaRetira(Fila* filaClientes);
+void filaLibera(Fila* filaClientes);
